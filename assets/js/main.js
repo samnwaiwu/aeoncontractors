a0aac15a9a85cd1996534ba693c4ebba1a13cfdc
Samuel <samnwaiwu@gmail.com>
2024-02-14 21:34:20 +0100

    updates on index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,12 @@
-
+/**
+* Template Name: KnightOne
+* Updated: May 30 2023 with Bootstrap v5.3.0
+* Template URL: https://bootstrapmade.com/knight-simple-one-page-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
 (function() {
   "use strict";
-
-
-/*** Alert Message*/
-setTimeout(wakeUpUser, 1000);
-function wakeUpUser() {
-alert("Welcome Samuel");
-}
 
   /**
    * Easy selector helper function
@@ -173,6 +172,62 @@ alert("Welcome Samuel");
     selector: '.glightbox'
   });
 
+  
+  /**
+   * Init swiper slider with 1 slide at once in desktop view
+   */
+  new Swiper('.slides-1', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+  /**
+   * Init swiper slider with 3 slides at once in desktop view
+   */
+  new Swiper('.slides-3', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40
+      },
+
+      1200: {
+        slidesPerView: 3,
+      }
+    }
+  });
+
+  
   /**
    * Porfolio isotope and filter
    */
@@ -230,5 +285,4 @@ alert("Welcome Samuel");
    */
   new PureCounter();
 
-}
-)()
+})()
